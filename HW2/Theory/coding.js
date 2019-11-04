3991566cdfc596a234ae5ab9f5afd41847b66f65
joshrymkiewicz <joshrymkiewicz@yahoo.com>
2019-11-04 09:52:57 -0700

    Add files via upload
    3308 hw2

---
+++ HW2/Theory/coding.js
@@ -0,0 +1,36 @@
+function display2_1() {
+  var myanswer  = "My answer goes here: 1. undefined true false. 2. == null is type conversion while, === is not a type conversion undefined is undefined while null is an object " // TODO 2.1: Fill in Answer here after the :
+  document.getElementById("21answer").innerHTML = myanswer; // Do not change
+}
+function display2_2() {
+  var myanswer  = "My answer goes here: while using 'use strict' you arent setting anything to x,even though it should be executed but if you were to take 'use strict' out you would be using it to 3.14" // TODO 2.2: Fill in Answer here
+  document.getElementById("22answer").innerHTML = myanswer; // Do not change
+}
+
+function display2_5() {
+  var myanswer  = "My answer goes here: within the html file we are getting from the elmenetId the children, the element is the divison within tag being the div_1134 and children are the tags in the division" // TODO 2.5: Fill in Answer here
+  document.getElementById("25answer").innerHTML = myanswer; // Do not change
+}
+
+function countDiv() { // Do not add or remove lines to this function
+    var divs = document.getElementsByTagName("div"); // TODO: Question 2.3: Use the document Object to get the number of divs in the HTML page. Replace dummyMethod with the correct one.
+    alert("Number of divs in this page is: " + divs.length); // Do not change
+}
+
+function makeOrange() { // Do not add or remove lines to this function
+    var narutos = document.getElementsByName("naruto"); // TODO: Question 2.4: Use the document Object to get all tags named "naruto" in the HTML page. Replace dummyMethod with the correct one.
+    for(var i=0; i< narutos.length; i++) { // Do not change
+        narutos[i].style.color = 'orange'; // Do not change
+    }
+}
+
+function getChildrenTags() { // Do not add or remove lines to this function
+  var children = document.getElementById("div_1134").children; // TODO: Question 2.5 Use the appropriate function to get all the children of a div named 'div_1134'
+  var tagNames = ""; // Do not change
+  for (var i = 0; i < children.length; i++) {// Do not change
+    console.log(children[i].tagName);// Do not change
+    tagNames += children[i].tagName +", " ;// Do not change
+  }// Do not change
+  alert(children.length); // Do not change
+  alert("The tags that I found are: " + tagNames);// Do not change
+}
